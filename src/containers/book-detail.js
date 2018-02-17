d9aea1ebaa4b4fefc0831f117c05396f3b0bdfab
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-17 09:49:44 -0500

    connected book-detail to redux reducers

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
   render() {
     return (
       <div>Book Detail!</div>
       );
   }
 }
+
+// Connect book detail to the redux store so it's told about changes to the
+// active book state
+
+// Argument is the application state. This will return props object that will be
+// used inside the BookDetail above.
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
