3a507348e6103396be92e4a138a59447670bc86b
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-17 10:17:16 -0500

    Added book page numbers to display

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Clean Code' },
-    { title: 'Learn Python the Hard Way' },
-    { title: 'The God of Small Things' },
-    { title: 'The Elegance of the Hedgehog' }
+    { title: 'Clean Code', pages: 450 },
+    { title: 'Learn Python the Hard Way', pages: 272 },
+    { title: 'The God of Small Things', pages: 500 },
+    { title: 'The Elegance of the Hedgehog', pages: 398 }
   ]
 }
