25fe1e8cfa89085a9eebcfecb7e7637516ea331b
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-17 09:41:15 -0500

    Corrected typo in book-detail

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 
-export default class BookDetail extends Component
+export default class BookDetail extends Component {
   render() {
     return (
       <div>Book Detail!</div>
       );
   }
+}
